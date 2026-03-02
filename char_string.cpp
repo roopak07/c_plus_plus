87945e56f4831e78fbce39e287918fbe20c7b577
Roopak <h.rupak61@gmail.com>
2026-03-02 12:13:31 +0530

    Create char_string.cpp

---
+++ char_string.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    int x=5, y=10;
+    char a = 'A';
+    std::string name = "Sum of x+y is:";
+    std::cout << name << x+y << "\n";
+    std::cout << "Charector:" << a;
+    return 0;
+}
