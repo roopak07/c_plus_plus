b7b80132268dd9b43e0c48cee363cac3c95b38bf
Roopak <h.rupak61@gmail.com>
2026-03-02 14:49:23 +0530

    Create type-def.cpp

---
+++ type-def.cpp
@@ -0,0 +1,11 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+typedef std::string disp;
+
+int main() {
+       disp name = "hello";
+       cout << name;
+    return 0;
+}
