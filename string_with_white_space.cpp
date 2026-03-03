2d2a4eb46d458ecee11fdb0cccc0c7ed731a54cf
Roopak <h.rupak61@gmail.com>
2026-03-03 14:14:23 +0530

    Create string_with_white_space.cpp

---
+++ string_with_white_space.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    
+    cout << "Enter your age: ";
+    cin >> age; // here cin will get new line so the program will terminate, so we need to use below get line to accept new line for the next charecter
+    
+    cout << "Enter your name: ";
+    //getline(cin, name); // wont accept white space if we read any string before tis
+    getline(cin >> ws, name); //This will accept white space if read any string before this    
+    cout << "Hello " << name << "\n";
+    cout << "Your " << age << " years old";
+}
