15c98658c7cdb64c54542c5809cf785e5a90226e
Roopak <h.rupak61@gmail.com>
2026-03-03 12:15:23 +0530

    Update string_with_spaces.cpp
    Removed std:: as we are using namespace

--- string_with_spaces.cpp
+++ string_with_spaces.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-using namespace std;
+using namespace std; //we can remove all the std:: as we are using this line
 int main()
 {
-    std::string name; 
+    //std::string name; 
+    string name
     cout << "Enter your name: ";
     //cin >> name; // This wont work if include any spaces in the string
-    std::getline(std::cin, name);
+    //std::getline(std::cin, name);
+    getline(cin, name);
     cout << "Enter your age: ";
     int age;
     cin >> age;
