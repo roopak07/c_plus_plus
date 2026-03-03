47a5e60a671f288c7e5a311c3764e7c5a7266dc6
Roopak <h.rupak61@gmail.com>
2026-03-03 11:36:58 +0530

    Create using1.cpp

---
+++ using1.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using str=std::string; // string is a alisa so we use like this
+//using print = std::cout; // we can to this as cout is a object not alisa
+using namespace std;
+int main() {
+  int x=10, y=20;
+  str sum_of_xy = "Sum of X+Y = ";
+  cout << sum_of_xy << x+y;
+}
