3198793c68bf96d395e77de415cebd4966a85fc8
Roopak <h.rupak61@gmail.com>
2026-03-03 14:44:10 +0530

    Create cmath.cpp

---
+++ cmath.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+// c functions available in c++ - https://cplusplus.com/reference/cmath/
+int main()
+{
+    int power, squar_root;
+    power = pow(2,3); // availabe in cmath libraray
+    squar_root = sqrt(4); // // availabe in cmath libraray
+    std::cout << "2 power 3 is: " << power << "\n";
+    std::cout << "Squar root of 4 is: " << squar_root;
+}
