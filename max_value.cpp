c2f93db52615ba0b739d396c84426778395d5a02
Roopak <h.rupak61@gmail.com>
2026-03-03 14:15:20 +0530

    Create max_value.cpp

---
+++ max_value.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    double a=50, b=100, z;
+    z=std::max(a,b); // This will compare a and b , provide largest value 
+    std::cout << "max value is: " << z;
+    return 0;
+}
